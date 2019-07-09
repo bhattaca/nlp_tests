d03f1402b17a0ca9199b520590f604d641fef6fe
Arindam Bhattacharya <arindam@uber.com>
2019-07-08 23:00:51 -0700

    Update select_dataset_col.py

--- select_dataset_col.py
+++ select_dataset_col.py
@@ -14,10 +14,33 @@ TAG_RE = re.compile(r'<[^>]+>')
 
 def remove_tags(text):
     return TAG_RE.sub('', text)
+def keep_alpha_numeric(text):
+    #return text
+    return re.compile('[\W_]+', re.UNICODE).sub(' ', text)
                       
-df=pd.read_csv('~/Downloads/report-3kHgNEBph-run-nXPEvxtw7.csv')
+df=pd.read_csv('~/Downloads/report-3kHgNEBph-run-Fkj5hnMYb.csv')
 print ( df.shape)
 clean_txt=pd.DataFrame([ remove_tags(str(row)) for row in df['content']])
-clean_txt.to_csv('~/Downloads/eaters_large_dataset.csv',index=False)
+clean_txt=pd.DataFrame([ keep_alpha_numeric(str(row)) for row in clean_txt.iloc[:,0]])
+clean_txt.to_csv('/Users/arindam/Downloads/driver_cancellation_dataset.csv',index=False)
+print ("done")
 
 
+
+
+#remove share detials tag
+text = open("/Users/arindam/Downloads/driver_cancellation_dataset.csv", "r")
+
+
+
+
+text = ''.join([i for i in text]) \
+    .replace("Share Details", "") \
+    .replace("::","") \
+    .replace(":","") \
+    .replace("&#39;","\'") \
+    .replace("\"","")
+         
+x = open("/Users/arindam/Downloads/driver_cancellation_datase_clean.csv","w")
+x.writelines(text)
+x.close()
